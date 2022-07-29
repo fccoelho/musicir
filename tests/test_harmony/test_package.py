c500053e004a6825a0c0119cf491e0ff3277ecab
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-07-28 21:06:53 -0300

    fixed bug in get scale notes and added function to get enharmonics

--- tests/test_harmony/test_package.py
+++ tests/test_harmony/test_package.py
@@ -1,10 +1,23 @@
 import unittest  # type: ignore
-from musicir.harmony import get_scale_notes
+
+from musicir.harmony import get_scale_notes, get_enharmonic
+
 
 class HarmonyPackageTestCase(unittest.TestCase):
-    def test_something(self):
-        notes = get_scale_notes('Mixolydian', 'C')
-        self.assertEqual(notes, ['C', 'D', 'E', 'F', 'G', 'A', 'A#'])
+    def test_get_scale_notes(self):
+        notes = get_scale_notes("Mixolydian", "C")
+        self.assertEqual(["C", "D", "E", "F", "G", "A", "B-"], notes)
 
-if __name__ == '__main__':
+    def test_get_scale_notes2(self):
+        notes = get_scale_notes("Harmonic Minor", "E-")
+        answer = ["E-", "F", "G-", "A-", "B-", "C-", "D"]
+        for i, n in enumerate(notes):
+            self.assertIn(n, [answer[i], get_enharmonic(answer[i]).upper()])
+
+    def test_get_enharmonic(self):
+        self.assertEqual('c', get_enharmonic('b#'))
+        self.assertEqual('b-', get_enharmonic('a#'))
+
+
+if __name__ == "__main__":
     unittest.main()
